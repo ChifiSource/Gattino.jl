5d8cd05a65507a50915026f4f4550179a545853b
emmy <52672675+emmettgb@users.noreply.github.com>
2022-06-27 06:08:49 -0500

    set default width -> 100%

--- src/Components.jl
+++ src/Components.jl
@@ -61,7 +61,8 @@ mutable struct DashBoard <: Servable
                 end
             end
             style!(page_div, "background-color" => "#1c2e4a",
-            "border-radius" => "15px", "padding" => "20px", "float" => "left")
+            "border-radius" => "15px", "padding" => "20px", "float" => "left",
+            "width" => "100%")
             push!(page_div, pages[1])
             navbar::Component = nav(pages, c, anim_out())
             write!(c, stylesvs)
