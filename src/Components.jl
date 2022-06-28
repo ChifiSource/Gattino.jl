b9e309765539c1f8d755cf6491b8478c8d82f421
emmy <52672675+emmettgb@users.noreply.github.com>
2022-06-28 15:20:31 -0500

    updated selected values

--- src/Components.jl
+++ src/Components.jl
@@ -277,7 +277,8 @@ Creates a number input component.
 ```
 """
 function numberinput(name::String, range::UnitRange = 1:10; value::Integer = 5)
-    input(name, type = "number", min = range[1], max = range[2])
+    input(name, type = "number", min = range[1], max = range[2],
+    selected = value, oninput = "`this.setAttribute('selected',this.value);`")
 end
 
 """
@@ -294,7 +295,8 @@ function rangeslider(name::String, range::UnitRange = 1:100;
                     value::Integer = 50, step::Integer = 5)
     input(name, type = "range", min = string(minimum(range)),
      max = string(maximum(range)), value = value, selected = value,
-            step = step)
+            step = step,
+            oninput = "`this.setAttribute('selected',this.value);`")
 end
 
 """
