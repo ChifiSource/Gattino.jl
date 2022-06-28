cbfc002f5a5d5ce5e40a17e2c1f8e285ae36bce4
emmy <52672675+emmettgb@users.noreply.github.com>
2022-06-28 15:11:42 -0500

    Add files via upload

--- src/Components.jl
+++ src/Components.jl
@@ -294,7 +294,7 @@ function rangeslider(name::String, range::UnitRange = 1:100;
                     value::Integer = 50, step::Integer = 5)
     input(name, type = "range", min = string(minimum(range)),
      max = string(maximum(range)), value = value, selected = value,
-            step = step, oninput = "'this.selected = this.value;'")
+            step = step)
 end
 
 """
