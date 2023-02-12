5bca44a13434853b70cc7159b4369d9665789b22
emmy <52672675+emmettgb@users.noreply.github.com>
2023-02-11 19:46:27 -0600

    refined Contexts

--- src/Contexts.jl
+++ src/Contexts.jl
@@ -1,14 +1,19 @@
-mutable struct Context <: Toolips.Servable
+mutable struct Context <: Toolips.Modifier
     window::Component{:svg}
-    dim::Tuple{Int64}
+    dim::Pair{Int64, Int64}
     margin::Pair{Int64, Int64}
+    Context(window::Component{:svg}, margin::Pair{Int64, Int644}) = new(window,
+    parse(window[:width], Int64) => parse(window[:height], int64), margin)
     function Context(width::Int64 = 1280, height::Int64 = 720,
         margin::Pair{Int64, Int64} = 0 => 0)
-
+        window::Component{:svg} = svg(randstring(10), width = width,
+        height = height)
+        Context(window, margin)::Context
     end
 end
 
-function context!(f::Function, window::Component{:svg})
+function context!(f::Function, window::Component{:svg},
+    margin::Pair{Int64, Int64} = 0 => 0)
 
 end
 
