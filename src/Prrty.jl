013fe7397e7b1868408167991c62f0894d3a945a
emmy <52672675+emmettgb@users.noreply.github.com>
2023-02-20 01:51:40 -0600

    added soon to be labeled grid

--- src/Prrty.jl
+++ src/Prrty.jl
@@ -3,12 +3,22 @@ using Toolips
 using ToolipsDefaults
 using ToolipsSVG
 using Contexts
-import Base: display, show
-include("Contexts.jl")
-include("Dashboards.jl")
+import Contexts: line!
+include("context_plotting.jl")
 
-plot(x::Any, y::Any; args ...) = begin
-
+scatter(x::Vector{<:Number}, y::Vector{<:Number}, width::Int64 = 500,
+height::Int64 = 500, margin::Pair{Int64, Int64} = 0 => 0; divisions::Int64 = 4,
+    title = "", args ...) = begin
+    con::Context = Context(width, height, margin)
+    group!(con, "plotbase") do g::Group
+        axes!(con)
+        grid!(con, divisions)
+    end
+    group!(con, "points") do g::Group
+        points!(con, x, y)
+    end
+    con
 end
 
+include("Dashboards.jl")
 end # module
