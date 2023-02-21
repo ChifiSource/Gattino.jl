e824f13de66ec301101c4d6393498be07f2cba09
emmy <52672675+emmettgb@users.noreply.github.com>
2023-02-21 13:36:47 -0600

    Add files via upload

--- src/Prrty.jl
+++ src/Prrty.jl
@@ -8,7 +8,7 @@ include("context_plotting.jl")
 
 scatter(x::Vector{<:Number}, y::Vector{<:Number}, width::Int64 = 500,
 height::Int64 = 500, margin::Pair{Int64, Int64} = 0 => 0; divisions::Int64 = 4,
-    title = "", args ...) = begin
+    title::String = "", args ...) = begin
     con::Context = Context(width, height, margin)
     group!(con, "plotbase") do g::Group
         axes!(con)
@@ -20,5 +20,10 @@ height::Int64 = 500, margin::Pair{Int64, Int64} = 0 => 0; divisions::Int64 = 4,
     con
 end
 
+line(x::Vector{<:Number}, y::Vector{<:Number}, width::Int64 = 500,
+height::Int64 = 500, margin::Pair{Int64, Int64} = 0 => 0; divisions::Int64 = 4,
+title::String = "", args ...) = begin
+    con::Context = Context(width, height, margin)
+end
 include("Dashboards.jl")
 end # module
