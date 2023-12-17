c247e8ac84504d3380e007a52cc68437a13bc72e
emmy <52672675+emmaccode@users.noreply.github.com>
2023-12-17 04:02:45 -0700

    negatives support; axislabels !

--- src/Gattino.jl
+++ src/Gattino.jl
@@ -46,6 +46,46 @@ function scatter_plot!(con::AbstractContext, x::Vector{<:Number}, y::Vector{<:Nu
     con::AbstractContext
 end
 
+function scatter_plot!(con::AbstractContext, x::Any, y::Any, colors::Vector{String} = ["blue", "orange"]; divisions::Int64 = 4, title::String = "")
+    if length(x) != length(y)
+      throw(
+          DimensionMismatch("x and y must be of the same length! got ($(length(x)), $(length(y)))")
+      )
+    end
+  w::Int64, h::Int64 = con.dim[1], con.dim[2]
+  ml::Int64, mt::Int64 = con.margin[1], con.margin[2]
+  if title != ""
+      group!(con, "title") do titlegroup::Group
+          posx = Int64(round(con.dim[1] * .35) + con.margin[1])
+          posy = Int64(round(con.dim[2] * .08) + con.margin[2])
+          text!(con, posx, posy, title, "fill" => "black", "font-size" => 15pt)
+      end
+      w, h = Int64(round(con.dim[1] * .75)), Int64(round(con.dim[2] * .75))
+      ml, mt = Int64(round(con.dim[1] * .12)) + con.margin[1], Int64(round(con.dim[2] * .12) + con.margin[2])
+  end
+  group(con, w, h, ml => mt) do plotgroup::Group
+    features = [col for col in eachcol(x)]
+      group!(plotgroup, "axes") do g::Group
+          axes!(g)
+      end
+      group!(plotgroup, "grid") do g::Group
+          grid!(g, divisions)
+      end
+      [begin
+        group!(plotgroup, "points") do g::Group
+            points!(g, feature, y)
+        end
+        end for feature in features]
+      group!(plotgroup, "labels") do g::Group
+          gridlabels!(g, x, y, divisions)
+      end
+      group!(plotgroup, "axislabels") do g::Group
+
+      end
+  end
+  con::AbstractContext
+end
+
 scatter(args ...; keyargs ...) = scatter_plot!(Context(500, 500), args ...; keyargs ...)
 
 function line_plot!(con::AbstractContext, x::Vector{<:Number}, y::Vector{<:Number}; divisions::Int64 = 4, title::String = "")
