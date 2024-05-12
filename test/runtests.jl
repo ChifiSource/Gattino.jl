14da54dd000b77d0a1274dc97f3dbad0b19afe89
emmy <52672675+emmaccode@users.noreply.github.com>
2024-05-11 18:45:43 -0600

    completed contexts tests!
    Just one more test set left, the `context_plotting` tests. Then I am going to write a *quick* README update, Release `ToolipsServables` `0.1.5` and then this will be ready!

--- test/runtests.jl
+++ test/runtests.jl
@@ -63,11 +63,74 @@ using Gattino
         @test grad[2] == "rgb(260, 5, 0)"
     end
     @testset "contexts" verbose = true begin
-        newcon = context()
-        @test typeof(newcon) <: Gattino.AbstractContext
-        
+        @testset "base contexts" begin
+            newcon = context()
+            @test typeof(newcon) <: Gattino.AbstractContext
+            newcon = context(100, 100, 5 => 5)
+            @test con.dim == 100 => 100
+            newcon = context(100, 100) do con::Context
+                points!(con, [5, 10, 15], [5, 10, 15])
+                group!(con, "sample") do g::Group
+                    text!(g, 5, 10, "hello!")
+                end
+            end
+            @test length(newcon.window[:children]) == 2
+            @test length(newcon.window["sample"][:children]) == 1
+        end
+        con = context()
+        @testset "groups" begin
+            g = group(con, 100, 50, 50 => 5) do g
+                group!(g, "text") do g2::Group
+                    text!(g, 2, 5, "hello")
+                end
+            end
+            @test string(con.window[:children]["text"][1][x]) == string(50 + 2 + 5)
+            @test length(g.window[:children]) == 1
+            @test length(con.window[:children]) == 1
+        end
+        @testset "cat layouts" begin
+            testcat = hcat(vcat(con, con), vcat(con, con))
+            @test typeof(testcat) == Component{:div}
+            f = findfirst(comp -> typeof(comp) == Component{:br}, testcat[:children])
+            @test ~(isnothing(f))
+            @test f == 3
+            @test length(testcat) == 5
+        end
+        @testset "layers" begin
+            @test "text" in [p[2] for p in layers(con)]
+            rename_layer!(con, "text", "label")
+            @test ~("text" in [p[2] for p in layers(con)])
+            @test "label" in [p[2] for p in layers(con)]
+            con2 = context(500, 500) do cont::Context
+                line!(con2, 5 => 6, 4 => 5)
+            end
+            @test length(layers(con2)) == 1
+            merge!(con, con2)
+            @test length(con.window[:children]) == 2
+            delete_layer!(con, "label")
+            @test ~("label" in [p[2] for p in layers(con)])
+            @test length(con.window[:children]) == 1
+            newcon = scatter([5, 10, 78], [20, 30, 10])
+            @test length(newcon[:children]["points"][:children]) == 3
+            set_shape!(newcon, "points", :star)
+            @test typeof(newcon.window[:children]["points"][:children][1]) == Component{:star}
+            style!(newcon, "grid", "stroke" => "orange")
+            @test contains(newcon.window[:children]["grid"][1]["style"], "stroke:orange;")
+            open_layer(newcon, "points") do ecomp
+                set!(ecomp, :r, 10)
+                
+            end
+            @test string(newcon[:children]["points"][1]["r"]) == "10"
+        end
+        @testset "compression" begin
+            size = sizeof(con)
+            compress!(con)
+            newsize = sizeof(con)
+            @test length(con.window[:children]) == 0
+            @test newsize < size
+        end
     end
     @testset "context plotting" verbose = true begin
-
+        
     end
 end
